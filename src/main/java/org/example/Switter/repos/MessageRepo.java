7f3b6eb5fe3426c06e11afe2a6a7af2a255abf69
nikita-zaga <nik.zagaynov2015@gmail.com>
2021-05-29 11:18:30 +0300

    Test
    Test

---
+++ src/main/java/org/example/Switter/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package org.example.Switter.repos;
+
+import org.example.Switter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
